5ee4733b90046f7828e8e76806f6a3d9ba1724fe
mariana-calado <marianacalado01@gmail.com>
2025-06-04 20:33:08 -0300

    de novo

--- bt.py
+++ bt.py
@@ -7,7 +7,7 @@ def exibir_tabuleiro(jogador, tabuleiro, ocultar=False):
     print("    " + "  ".join(str(i) for i in range(10)))
     for i, linha in enumerate(tabuleiro):
         if ocultar:
-            print(f"{i}" + " ".join(["🌊" if celula == "⛵" else celula for celula in linha]))
+            print(f"{i}  " + " ".join(["🌊" if celula == "⛵" else celula for celula in linha]))
         else:
             print(f"{i}  " + " ".join(linha))
     print()
@@ -19,17 +19,17 @@ def posicao():
     print("Escolha a posição das suas embarcações!")
     for i in range(5):
         while True:
-                linha = int(input(f"Escolha a linha para embarcação {i+1} (0-9): "))
-                coluna = int(input(f"Escolha a coluna para embarcação {i+1} (0-9): "))
+            linha = int(input(f"Escolha a linha para embarcação {i+1} (0-9): "))
+            coluna = int(input(f"Escolha a coluna para embarcação {i+1} (0-9): "))
                 
-                if 0 <= linha < 10 and 0 <= coluna < 10:
-                    if tabuleiro_humano[linha][coluna] == "🌊":
-                        tabuleiro_humano[linha][coluna] = "⛵"
-                        break
-                    else:
-                        print("Você já tem um navio nesta coordenada! 🧭")
+            if 0 <= linha < 10 and 0 <= coluna < 10:
+                if tabuleiro_humano[linha][coluna] == "🌊":
+                    tabuleiro_humano[linha][coluna] = "⛵"
+                    break
                 else:
-                    print("Fora dos limites do mapa! 🗺️")
+                    print("Você já tem um navio nesta coordenada! 🧭")
+            else:
+                print("Fora dos limites do mapa! 🗺️")
 
 def posicao_computador():
     for _ in range(5):
